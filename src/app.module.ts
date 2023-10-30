93459fe46d54e238eb60ef6ec47fe3662a994e24
FabriceBartrolich <fabrice.bartrolich@sncf.fr>
2023-10-30 11:19:12 +0100

    feat: ajout des dépendances et initialisation des 3 ressources [back]

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { GenreModule } from './genre/genre.module';
+import { ProductModule } from './product/product.module';
+
+@Module({
+  imports: [
+     ConfigModule.forRoot({ envFilePath: ['.env'] }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      entities: [],
+      synchronize: false,
+    }),
+    UserModule,
+    GenreModule,
+    ProductModule,
+
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
